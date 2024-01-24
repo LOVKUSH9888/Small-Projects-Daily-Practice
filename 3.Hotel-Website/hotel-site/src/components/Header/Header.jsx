c50d048a8cb3ca99f2d491587d85fb86baed90e8
LOVKUSH9888 <lovkushyadav9888@gmail.com>
2024-01-24 18:51:18 +0530

    completed the header section completly

--- 3.Hotel-Website/hotel-site/src/components/Header/Header.jsx
+++ 3.Hotel-Website/hotel-site/src/components/Header/Header.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 const Header = () => {
   return (
     <>
-    <section id="header">
+      <section id="header">
+        {/* Header Container */}
         <div className="container">
-            <h1 className='text-white'>Travel-World</h1>
-            <button className='login-btn'>Login</button>
+          <h1 className="text-white">Travel-World</h1>
+          <button className="login-btn">Login</button>
         </div>
-    </section>
+        {/* Mid section text */}
+        <div className="container-2">
+          <h1>Travel Accross The Glove</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio,
+            vero eum fuga quis aut deserunt harum.
+          </p>
+          {/* Button */}
+          <div className="input-group ">
+            <input type="text" placeholder="Search" className="form-control"/>
+            <div className="input-group-append">
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
