0e95bf5e48c5792cd4a0ce51ce8586a7d08c5c39
LOVKUSH9888 <lovkushyadav9888@gmail.com>
2024-01-24 18:09:27 +0530

    working on the Travel-Hotel-site

---
+++ 3.Hotel-Website/hotel-site/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <>
+    <section id="header">
+        <div className="container">
+            <h1 className='text-white'>Travel-World</h1>
+            <button className='login-btn'>Login</button>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Header
