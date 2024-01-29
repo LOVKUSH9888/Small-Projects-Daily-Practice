d866bb949cbf86681ab169f942f4e8703e9f4d1e
LOVKUSH9888 <lovkushyadav9888@gmail.com>
2024-01-29 15:18:27 +0530

    working on the 4th project Business Website

---
+++ 4. Business-Website = Text on carousel/business-web/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./Navbar.css";
+function BasicExample() {
+  return (
+    <div className="section">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <a href="#">
+            <img src="/img/logo.png" alt="logo" />
+          </a>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default BasicExample;
